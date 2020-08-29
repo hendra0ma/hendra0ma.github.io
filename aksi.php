7097145390bcc6831d34584b8e6f477d46919482
hendra0ma <hendra0maulidan@gmail.com>
2020-08-29 12:06:08 +0700

    coba3

--- aksi.php
+++
@@ -1,5 +0,0 @@
-<?php 
-if (isset($_POST['input'])) {
-	echo $_POST['nama'];
-}
- ?>
--- js/script.js
+++
@@ -1,6 +0,0 @@
-var nama = document.getElementById("nama");
-var container = document.getElementById("container");
-
-nama.addEventListener("keyup", function() {
-    container.innerHTML = nama.value;
-});
